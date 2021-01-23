378fc9a79dcf51fb6e82ada8d109c1c4b5fa7b4f
kaylaorozco <66231133+kaylaorozco@users.noreply.github.com>
2021-01-23 13:54:25 -0600

    comfort link added

--- js/script.js
+++ js/script.js
@@ -5,11 +5,6 @@
 
 // redirected to comfort notifications
 
-$(".btn-armed").click(function(){
-    $(this).text('Contacting Authorities...');
-
-    var delay = 6500;
-    var url = 'https://kaylaorozco.github.io/HW_22/comfort.html'
-    setTimeout(function(){
-        window.location.href = url; }, delay );
-    })
+function redirect () {
+    window.location.href = 'https://kaylaorozco.github.io/HW_22/comfort.html';
+}
